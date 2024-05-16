c46be7c9a60f627b022abad6e57286dbd9bb52da
Andy608 <Andy608@users.noreply.github.com>
2024-05-15 18:00:58 -0700

    Initial setup for CrazyOres v3

---
+++ src/client/java/com/andysmods/crazyores/CrazyOresClient.java
@@ -0,0 +1,10 @@
+package com.andysmods.crazyores;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CrazyOresClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/com/andysmods/crazyores/CrazyOresDataGenerator.java
@@ -0,0 +1,11 @@
+package com.andysmods.crazyores;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CrazyOresDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
